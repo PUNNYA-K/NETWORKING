1c84f4d586a29a339f5144d6d7ddda13b2fcda24
Punnya.K <121844763+PUNNYA-K@users.noreply.github.com>
2024-04-09 15:16:55 +0530

    completed

---
+++ compare.sh
@@ -0,0 +1,30 @@
+#compare two files and delete duplicate
+
+if [ $# -ne 2 ]
+then 
+echo "Syntax is <$0> <file1> <file2>"
+elif [ -f $1 -a -f $2 ]
+then
+cmp $1 $2 >/dev/null
+if [ $? -eq 0 ]
+then 
+echo "the files $1 and $2 are same"
+rm -f $1
+echo "$1 deleted"
+else
+echo "$1 and $2 are not same"
+fi
+else
+echo "files are not ordinary files"
+fi
+
+
+
+
+
+
+
+
+
+
+
